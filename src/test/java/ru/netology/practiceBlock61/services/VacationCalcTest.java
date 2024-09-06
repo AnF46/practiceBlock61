5d3acd7b202342cb19a3ccf7cf5ad1ac000ba686
AnF <anfwork007@gmail.com>
2024-09-07 00:17:57 +0300

    CSV file added

--- src/test/java/ru/netology/practiceBlock61/services/VacationCalcTest.java
+++ src/test/java/ru/netology/practiceBlock61/services/VacationCalcTest.java
@@ -2,23 +2,23 @@ package ru.netology.practiceBlock61.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationCalcTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "Small Salary, 3, 10000, 3000, 20000",
-            "Big Salary, 2, 100000, 60000, 150000"
-    })
-
-    public void test(String test, int expected, int income, int expenses, int threshold){
+//    @CsvSource({
+//            "Small Salary, 3, 10000, 3000, 20000",
+//            "Big Salary, 2, 100000, 60000, 150000"
+//    })
+    @CsvFileSource(files = "src/test/resources/testDataNumbers.csv")
+    public void test(String test, int expected, int income, int expenses, int threshold) {
         VacationCalc service = new VacationCalc();
 
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
-
 
     }
 
